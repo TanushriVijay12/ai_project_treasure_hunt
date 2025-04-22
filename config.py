bcc581d2bef795788020344b83b9032099e0bfef
TanushriVijay12 <tanushri.fall24@gmail.com>
2025-04-22 13:40:55 -0400

    Updated config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+TILE_SIZE = 80
+GRID_SIZE = 5
+WINDOW_PADDING = 50
+
+SCREEN_WIDTH = GRID_SIZE * TILE_SIZE
+SCREEN_HEIGHT = GRID_SIZE * TILE_SIZE
+
+COLORS = {
+    'fog': (30, 30, 30),
+    'safe': (200, 200, 200),
+    'agent': (0, 128, 255),
+    'trap': (255, 0, 0),
+    'treasure': (255, 215, 0),
+    'grid': (50, 50, 50),
+}
